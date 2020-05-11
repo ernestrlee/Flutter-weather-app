76cf75656bf4e7c7a29fad8dd84fa0820039088b
Ernest-Lee <ernestrlee@yahoo.com>
2020-05-11 01:55:19 -0700

    Added init state to get location when initializing loading screen. Removed scaffold content.

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -8,6 +8,11 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
 
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
   void getLocation() async {
     Position position = await Geolocator().getLastKnownPosition(desiredAccuracy: LocationAccuracy.low);
     print(position);
@@ -16,14 +21,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            getLocation();
-          },
-          child: Text('Get Location'),
-        ),
-      ),
+
     );
   }
 }
