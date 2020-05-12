eeb9e13c5cfba3cd92cc677695200f157d398c0e
Ernest-Lee <ernestrlee@yahoo.com>
2020-05-12 01:14:20 -0700

    Added http package. Added code to get sample weather API.

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
+import 'package:http/http.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,23 +8,35 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-
   void initState() {
     super.initState();
     getLocation();
+    getData();
   }
 
-void getLocation() async {
+  void getLocation() async {
     Location location = Location();
     await location.getCurrentLocation();
     print(location.latitude);
     print(location.longitude);
-}
+  }
+
+  void getData() async {
+    Response response = await get(
+        'https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=439d4b804bc8187953eb36d2a8c26a02');
+    print(response.statusCode);
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+      print(data);
+    }
+    else {
+      print(response.statusCode);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-
-    );
+    return Scaffold();
   }
 }
