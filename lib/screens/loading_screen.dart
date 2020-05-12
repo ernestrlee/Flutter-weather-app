fc6fe0d3883ebcea5739ea96aa4aa2c18710f37d
Ernest-Lee <ernestrlee@yahoo.com>
2020-05-12 01:51:11 -0700

    Added dart convert package for json data.

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -29,6 +30,9 @@ class _LoadingScreenState extends State<LoadingScreen> {
     if (response.statusCode == 200) {
       String data = response.body;
       print(data);
+
+      var decodedData = jsonDecode(data);
+
     }
     else {
       print(response.statusCode);
