82bcb52f8c3421efdd8843e7babf4b50d5f99d6b
Ernest-Lee <ernestrlee@yahoo.com>
2020-05-12 01:15:12 -0700

    Added package name for http.

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -22,7 +22,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   void getData() async {
-    Response response = await get(
+    http.Response response = await http.get(
         'https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=439d4b804bc8187953eb36d2a8c26a02');
     print(response.statusCode);
 
