4461ec626d894c0abd037b92a6b7e16a2cb4afae
Ernest-Lee <ernestrlee@yahoo.com>
2020-05-14 01:12:32 -0700

    Added flutter spinkit plugin. Added spinning graphic for loading screen.

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
 import 'package:clima/services/networking.dart';
 import 'package:clima/screens/location_screen.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 const apiKey = 'e72ca729af228beabd5d20e3b7749713';
 
@@ -42,6 +43,13 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: SpinKitDoubleBounce(
+          color: Colors.white,
+          size: 100.0,
+        ),
+      ),
+    );
   }
 }
