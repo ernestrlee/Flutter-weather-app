629b503f8f2742b3fbc9e2904a1593d4ab59f357
Ernest-Lee <ernestrlee@yahoo.com>
2020-05-14 21:11:32 -0700

    Fixed bug in url, long to lon. Changed from metric units to imperial.

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -7,15 +7,10 @@ const openWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
 class WeatherModel {
 
   Future<dynamic> getLocationWeather() async{
-    print('getting location');
-
     Location location = Location();
     await location.getCurrentLocation();
 
-    print(location.longitude);
-    print(location.latitude);
-
-    NetworkHelper networkHelper = NetworkHelper('$openWeatherMapURL?lat=${location.latitude}&long=${location.longitude}&appid=$apiKey&units=metric');
+    NetworkHelper networkHelper = NetworkHelper('$openWeatherMapURL?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=imperial');
 
     var weatherData = await networkHelper.getData();
 
